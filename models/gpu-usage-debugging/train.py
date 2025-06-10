0143bfff2ed81e7e2be94f20a701fd5d32c4bd21
Kaan Özaltan <kaan.oezaltan@rwth-aachen.de>
2025-06-11 01:24:20 +0200

    Refactor

--- models/gpu-usage-debugging/train.py
+++ models/gpu-usage-debugging/train.py
@@ -7,10 +7,10 @@ import wandb
 from piano_transformer.config import *
 from piano_transformer.datasets.dataset import build_collator, build_datasets
 from piano_transformer.datasets.preprocessing import split_datasets_into_chunks
-from piano_transformer.model.mistral_model import build_mistral_model
-from piano_transformer.tokenization.tokenizer import create_remi_tokenizer
-from piano_transformer.training.trainer import make_trainer
-from piano_transformer.midi import get_midi_file_lists
+from piano_transformer.model import build_mistral_model
+from piano_transformer.tokenizer import create_remi_tokenizer
+from piano_transformer.trainer import make_trainer
+from piano_transformer.utils.midi import get_midi_file_lists
 
 ## VERSION INFO
 
