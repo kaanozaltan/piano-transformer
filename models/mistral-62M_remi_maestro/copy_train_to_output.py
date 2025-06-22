e12cb238c9641298fec541bb24cbf713cc9b171e
Jonathan Lehmkuhl <jonathan.lehmkuhl@rwth-aachen.de>
2025-06-22 21:47:01 +0200

    Improve generation script (move to gpu, allow from_scratch_generation) + Make mgeval work and add functions for absolute and relative measurements of high-level music features

---
+++ models/mistral-62M_remi_maestro/copy_train_to_output.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+import shutil
+
+src_dir = Path(
+    "/hpcwork/lect0148/experiments/mistral-62M_remi_maestro_v2/data_processed/maestro_train"
+)
+dst_dir = Path("/hpcwork/lect0148/experiments/mistral-62M_remi_maestro_v2/output/train")
+
+dst_dir.mkdir(parents=True, exist_ok=True)
+
+for i, file in enumerate(src_dir.rglob("*.midi")):
+    if file.is_file():
+        new_name = f"{i}_prompt.midi"  # keeps original filename and extension
+        target_file = dst_dir / new_name
+        shutil.copy2(file, target_file)
