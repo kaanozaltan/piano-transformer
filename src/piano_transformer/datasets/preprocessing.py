dc8a60682fd758e15edde244568a68dd44ba1c4e
Jonathan Lehmkuhl <jonathan.lehmkuhl@rwth-aachen.de>
2025-07-08 18:46:43 +0200

    Fix preprocessing bug

--- src/piano_transformer/datasets/preprocessing.py
+++ src/piano_transformer/datasets/preprocessing.py
@@ -5,20 +5,22 @@ from miditok.utils import split_files_for_training
 
 
 def split_datasets_into_chunks(
-        midi_files_lists: list[Path],
-        tokenizer: MusicTokenizer,
-        processed_path: Path,
-        dataset_name: str,
-        max_seq_len: int,
-        num_overlap_bars: int,
-        overwrite: bool = False,
+    midi_files_lists: list[Path],
+    tokenizer: MusicTokenizer,
+    processed_path: Path,
+    dataset_name: str,
+    max_seq_len: int,
+    num_overlap_bars: int,
+    overwrite: bool = False,
 ):
     chunks_lists = {}
     for split in ["train", "validation", "test"]:
         save_dir = processed_path / f"{dataset_name}_{split}"
 
         if save_dir.exists() and not overwrite:
-            print(f"Skipping {split} split, found existing data at {save_dir.resolve()}")
+            print(
+                f"Skipping {split} split, found existing data at {save_dir.resolve()}"
+            )
         else:
             print(f"Processing {split} split and saving to {save_dir.resolve()}")
             split_files_for_training(
@@ -29,5 +31,10 @@ def split_datasets_into_chunks(
                 num_overlap_bars=num_overlap_bars,
             )
 
-        chunks_lists[split] = list((processed_path / f"{dataset_name}_{split}").glob("**/*.midi"))
+        chunks_lists[split] = list(
+            (processed_path / f"{dataset_name}_{split}").glob("**/*.midi")
+        )
+        chunks_lists[split] += list(
+            (processed_path / f"{dataset_name}_{split}").glob("**/*.mid")
+        )
     return chunks_lists
