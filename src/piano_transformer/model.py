ad813842312e98a4207c2376a8eb02b9c2d5ccd8
jonathanlehmkuhl <jonathanlehmkuhl@gmail.com>
2025-07-13 16:16:13 +0200

    Compute intermediate_size and num_attention_heads from other hyperparameters and set dropout and max_embeddings fixed

--- src/piano_transformer/model.py
+++ src/piano_transformer/model.py
@@ -12,17 +12,15 @@ def build_mistral_model(
         vocab_size=len(tokenizer),
         num_hidden_layers=cfg["num_hidden_layers"],
         hidden_size=cfg["hidden_size"],
-        intermediate_size=cfg["intermediate_size"],
-        num_attention_heads=cfg["num_attention_heads"],
-        num_key_value_head=cfg[
-            "num_attention_heads"
-        ],  # Assuming num_key_value_head is same as num_attention_heads
+        intermediate_size=cfg["hidden_size"] * 4,
+        num_attention_heads=cfg["hidden_size"] // 64,
+        num_key_value_head=cfg["hidden_size"] // 64,
         sliding_window=max_seq_len,  # Use max_seq_len as sliding window size
-        max_position_embeddings=cfg["max_position_embeddings"],
+        max_position_embeddings=8192,
         pad_token_id=tokenizer["PAD_None"],
         bos_token_id=tokenizer["BOS_None"],
         eos_token_id=tokenizer["EOS_None"],
-        attention_dropout=cfg["attention_dropout"],
+        attention_dropout=0.1,
         load_best_model_at_end=True,
         save_total_limit=2,
     )
